41f9f99818a1dc893e72e6ad278043e12d95d4e1
Mikołaj Walczak <mikiwalczak@gmail.com>
2017-03-30 10:09:03 +0200

    Move default bootloader before overwriting

--- install_bootloader.sh
+++ install_bootloader.sh
@@ -8,8 +8,12 @@ DEVICE=/dev/c0d0
 echo "Compiling bootloader"
 nasm ${BOOTLOADER_ASM_FILE} -f bin -o ${BOOTLOADER_FILE} || return 1
 
+echo "Moving default bootloader"
+dd bs=512 count=1 if=/dev/c0d0 of=tmp
+dd bs=512 count=1 if=tmp of=/dev/c0d0 seek=1
+
 echo "Trying to install bootloader..."
-dd bs=512 count=1 if=$BOOTLOADER_FILE of=$DEVICE
+dd bs=446 count=1 if=$BOOTLOADER_FILE of=$DEVICE
 
 echo "Succesfully installed bootloader: "
-dd bs=512 count=1 if=$DEVICE | od -Ax -tx1 -v
+dd bs=446 count=1 if=$DEVICE | od -Ax -tx1 -v
